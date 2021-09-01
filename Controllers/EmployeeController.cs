6f053d620f05e245081e53e7b12d551cd0dc323d
SanjeshMulmi <sanjeshmulmi@gmail.com>
2021-09-01 18:44:13 -0500

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebbApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebbApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository )
+        {
+            _employeeRepository = employeeRepository;
+        }
+        [HttpGet]
+        public  ActionResult GetEmployees()
+        {
+
+            return Ok(_employeeRepository.GetEmployees());
+
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddEmployees(Employee emp)
+        {
+
+
+            return Ok(await _employeeRepository.AddEmployee(emp));
+        }
+        
+    }
+}
---
+++ Models/EmployeeRepositoryAdo.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbApi.Models
+{
+    public class EmployeeRepositoryAdo : IEmployeeRepository
+    {
+
+        string connectionString = "data source=(localdb)\\MSSQLLocalDB; database=EmployeeDepartMent; Integrated Security=true;";
+        /*  string connectionS  "EmpDBConnection": "Server=(localdb)\\MSSQLLocalDB;Database=EmployeeDepartMent;Trusted_Connection=True;MultipleActiveResultSets=true"*/
+
+        SqlConnection con = null; 
+            
+        
+
+        public EmployeeRepositoryAdo()
+        {
+             con = new SqlConnection(connectionString);
+
+        }
+
+        public Task<Employee> AddEmployee(Employee emp)
+        {
+            /*List<Employee> employeeList = new List<Employee>();
+
+            SqlCommand cmd = new SqlCommand("InsertEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var employee = new Employee();
+
+                employee.EmployeeID = Convert.ToInt32(rdr["EmployeeID"]);
+                employee.FirstName = rdr["FirstName"].ToString();
+                employee.gender = (eGender) rdr["gender"];
+                employee.DepartMentID = Convert.ToInt32(rdr["DepartMentId"]);
+              
+              
+                employeeList.Add(employee);
+            }con.Close();*/
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEmployee(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployee(int employeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  List<Employee> GetEmployees()
+        {
+            List<Employee> employeeList = new List<Employee>();
+
+            SqlCommand cmd = new SqlCommand("select e.EmployeeID, e.FirstName, e.LastName, e.gender, e.DepartMentID, d.DepartMentName from Employees e inner join DepartMent d on e.DepartMentID = d.DepartMentID", con);
+            
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var employee = new Employee();
+
+                employee.EmployeeID = Convert.ToInt32(rdr["EmployeeID"]);
+                employee.FirstName = rdr["FirstName"].ToString();
+                employee.LastName = rdr["LastName"].ToString();
+                employee.gender = (eGender)rdr["gender"];
+                employee.DepartMentID = Convert.ToInt32(rdr["DepartMentId"]);
+                Department objDepart = new Department();
+                objDepart.DepartmentID = Convert.ToInt32(rdr["DepartMentId"]);
+                objDepart.DepartMentName = Convert.ToString(rdr["DepartMentName"]);
+                employee.departMent = objDepart;
+                /*employee.departMent.DepartmentID = Convert.ToInt32(rdr["DepartMentId"]);
+                employee.departMent.DepartMentName = Convert.ToString(rdr["DepartMentName"]);*/
+
+                // we cannot get the instance of connection multiple times. 
+                /* int deptID = employee.DepartMentID;
+                 // Sql command for Department Table
+                 SqlCommand cmdForDepart = new SqlCommand("SELECT DepartmentName FROM Department WHERE DepartmentID=" + deptID, con);
+                 cmdForDepart.CommandType = CommandType.Text;
+                 con.Open();
+                 SqlDataReader readDepart = cmdForDepart.ExecuteReader();
+                 employee.departMent = (Department)readDepart["DepartMentName"];*/
+
+
+                employeeList.Add(employee);
+            }
+            con.Close();
+            return  employeeList;
+        }
+
+        public Task<IEnumerable<Employee>> Search(string Name, eGender? gender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployee(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
